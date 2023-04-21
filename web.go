eff0c87dbceffdf99e15edd8501674d6787f085c
janeoa <asetmalik@gmail.com>
2023-04-21 13:15:46 +0600

    FIX: gin in new thread

--- web.go
+++ web.go
@@ -9,7 +9,139 @@ import (
 	"runtime"
 
 	"github.com/fatih/color"
+	"github.com/gin-gonic/gin"
 )
+
+func init_gin() {
+	color.Yellow("GUI is needed")
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.tmpl", gin.H{
+			"isConnected": Arduino.isConnected,
+			"mode":        Arduino.mode,
+		})
+	})
+	// if Arduino.isConnected {
+	r.Static("/static", "./static")
+	// }
+	r.GET("/changeModeToSlider", func(c *gin.Context) {
+		Arduino.mode = "slider"
+		c.HTML(200, "index.tmpl", gin.H{
+			"isConnected": Arduino.isConnected,
+			"mode":        Arduino.mode,
+		})
+	})
+
+	r.GET("/changeModeToJoystick", func(c *gin.Context) {
+		Arduino.mode = "joystick"
+		c.HTML(200, "index.tmpl", gin.H{
+			"isConnected": Arduino.isConnected,
+			"mode":        Arduino.mode,
+		})
+	})
+
+	r.GET("/status", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"isConnected": Arduino.isConnected,
+			"mode":        Arduino.mode,
+		})
+	})
+	r.GET("/action/on", func(ctx *gin.Context) {
+		EasyTransferSend(port, commandPack{Action: byte(CTurnOn)})
+		if Arduino.isConnected {
+			ctx.JSON(200, gin.H{
+				"status": "ok",
+				"action": "on",
+			})
+		} else {
+			ctx.JSON(500, gin.H{"error": "not connected"})
+		}
+	})
+	r.GET("/action/off", func(ctx *gin.Context) {
+		EasyTransferSend(port, commandPack{Action: byte(CTurnOff)})
+		if Arduino.isConnected {
+			ctx.JSON(200, gin.H{
+				"status": "ok",
+				"action": "off",
+			})
+		} else {
+			ctx.JSON(500, gin.H{"error": "not connected"})
+		}
+	})
+	r.GET("/action/horn", func(ctx *gin.Context) {
+		EasyTransferSend(port, commandPack{Action: byte(CHorn)})
+		if Arduino.isConnected {
+			ctx.JSON(200, gin.H{
+				"status": "ok",
+				"action": "horn",
+			})
+		} else {
+			ctx.JSON(500, gin.H{"error": "not connected"})
+		}
+	})
+	r.GET("/action/speedDown", func(ctx *gin.Context) {
+		EasyTransferSend(port, commandPack{Action: byte(CSmaller)})
+		if Arduino.isConnected {
+			ctx.JSON(200, gin.H{
+				"status": "ok",
+				"action": "speed Down",
+			})
+		} else {
+			ctx.JSON(500, gin.H{"error": "not connected"})
+		}
+	})
+	r.GET("/action/speedUp", func(ctx *gin.Context) {
+		EasyTransferSend(port, commandPack{Action: byte(CBigger)})
+		if Arduino.isConnected {
+			ctx.JSON(200, gin.H{
+				"status": "ok",
+				"action": "speed Up",
+			})
+		} else {
+			ctx.JSON(500, gin.H{"error": "not connected"})
+		}
+	})
+	r.GET("/action/:ch_name/:value", func(ctx *gin.Context) {
+		var command commandPack
+		if err := ctx.ShouldBindUri(&command); err != nil {
+			ctx.JSON(400, gin.H{"error": "could not bind command", "msg": err})
+			return
+		}
+		if command.Ch_name > 4 {
+			ctx.JSON(400, gin.H{"msg": "channel name out of bound [1..4]"})
+			return
+		}
+		ctx.JSON(200, gin.H{
+			"status":  "ok",
+			"command": CSetCh,
+			"ch_name": command.Ch_name,
+			"value":   command.Value,
+		})
+		if command.Value > 238 {
+			command.Value = 238
+		}
+		if command.Value < 56 {
+			command.Value = 56
+		}
+		switch command.Ch_name {
+		case 0:
+			Arduino.forward = command.Value
+		case 1:
+			Arduino.right = command.Value
+		}
+		command.Action = byte(CSetCh)
+		EasyTransferSend(port, command)
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	openbrowser("http://localhost:8080")
+	r.Run()
+}
 
 type Page struct {
 	Title string
